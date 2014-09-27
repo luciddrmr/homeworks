799f1f685e06a353c6b1fb3ed302b18b2013af0f
Bradley M. Kuhn <bkuhn@ebb.org>
2014-09-27 16:31:40 -0400

    Description of what we are trying to do with jointly developed my-string-contains

---
+++ jointly-developed-my-string-contains.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname jointly-developed-my-string-contains) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+; jointly-developed-my-string-contains
+;   This is the version of my-string-contains that Carol and Bradley developed together,
+;    applying our list of design patterns.
+
