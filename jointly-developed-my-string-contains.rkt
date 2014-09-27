e28cc9ebb91d20f00c5ee9dcb7b8f9727cad41d9
Bradley M. Kuhn <bkuhn@ebb.org>
2014-09-27 16:56:05 -0400

    Wrote a simple version of our desired function, that makes assumptions.
    This soltuion only works for the cases where str1 is exactly length 1, but
    it works.  So, the question is, how do we extroplate from this into something
    that solves the whole problem?

--- jointly-developed-my-string-contains.rkt
+++ jointly-developed-my-string-contains.rkt
@@ -12,11 +12,31 @@
 ;   Determines whether the first string appears literally in the second one. 
 
 
-(define my-string-contains? (lambda str1 str2)
+(define my-string-contains? (lambda (str1 str2) str1))
   ; Probably we're done if there's no str2 left.
   
   
 ;Sub problems:
   ; we need an expression that returns true iff. the first character of string1 and string2 are equivalent.
-(equal? (substring str1 0 1) (substring str2 0 1))
+;     (equal? (substring str1 0 1) (substring str2 0 1))
+  
+  
+; Separate and easier problem:
+   ; What happens when str1 is one character long?  Does that make thing easier?
+  
+  ; my-string-contains-one-char-only?  requires its first argument to be a string of length 1
+  ;  and return true iff. that character appears in str2 at least once.
+  
+(define my-string-contains-one-char-only? (lambda (short-string str2)
+   (cond [ (not (equal? (string-length short-string) 1)) (error "function only works when first argument is a string of length 1")]
+         [ (equal? str2 "") false ]
+         [ (equal? short-string (string-ith str2 0)) true]
+         [ else (my-string-contains-one-char-only? short-string (substring str2 1 (string-length str2)))])))
+  
+  
+"my-string-contains-one-char-only? test 1"
+true
+(my-string-contains-one-char-only? "a" "bccjguagjghu")
+  
+  
   
