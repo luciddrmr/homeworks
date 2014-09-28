645de673c3a8cde6545bd80c66c6fa8343c46c96
Carol <carol@fossygirl.com>
2014-09-28 10:05:46 -0700

    New files to figure out how the tests should look

---
+++ figuring-out-how-substring-works.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname figuring-out-how-substring-works) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+"Test 1"
+"ello"
+(substring "hello world" 1 5)
+"Test 2"
+"error: my-subtring expects a non-empty string"
+(substring "" 1 5)
+"Test 3"
+"error: requested substring is longer than string"
+(substring "c" 0 3)
+"Test 4"
+
+(substring "cat in the hat" 0 0)
