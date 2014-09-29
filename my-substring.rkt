01788f5a06efdac7d81cf7f52c80df35bdb204a7
Carol Smith <carols@carols-macpro2.roam.corp.google.com>
2014-09-29 12:07:08 -0700

    I think maybe I need to iterate in the recursion first and later in the helper, since one will be producing my string and one will be using the helper to start

--- my-substring.rkt
+++ my-substring.rkt
@@ -10,11 +10,12 @@
          [ (and (equal? num1 0) (equal? num2 0)) ""]
          [ (> (num2) (string-length str) "error: my-substring ending index is out of range" ]
          [ (equal? num1 num2) (string-ith str num1)]
-         [else (my-substring-helper str 0)]
+         [else (my-substring-helper str (string-ith num1) (string-ith num2))]
 
 (define my-substring-helper (lambda (str num1 num2)
-         (cond [ (equal? num2                    
-        [ else (implode (my-substring-helper str (+ 1 (string-ith num1)) (string-ith num2))) ])))
+         (cond [ (equal? num2)]      
+         [ 
+         [ else (implode (my-substring-helper str (+ 1 (string-ith num1)) (string-ith num2))) ])))
 
 "Test 1"
 "ello"
