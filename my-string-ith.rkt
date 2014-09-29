52d1f29b8b55849c6d00c240e88b7c8a16dd9161
Carol <carol@fossygirl.com>
2014-09-29 13:34:02 -0700

    Working on adding tests and cleaning up code

--- my-string-ith.rkt
+++ my-string-ith.rkt
@@ -4,12 +4,22 @@
 ;Write your own recursive version of string-ith.   Not using string-ith
 ;of course.
 
-(define my-function (lambda (arg1 arg2)
+(define my-string-ith (lambda (str num1)
     (my-function-helper arg1 arg2 arg3)))
 
-(define my-function-helper (lambda (arg1 arg2 arg3)
-    (let ( [ some-var (some-expr) ]
-           [ another-var (another-expr) ] )
+(define my-string-ith-helper (lambda (arg1 arg2 arg3)
         (cond  [ test base-case ]
                [ test2 another-base-case ]
                [ else recursion-expression ]))))
+
+"Test 1"
+(string-ith "cat" 0)
+
+"Test 2"
+(string-ith "hamburger" 10)
+
+"Test 3"
+(string-ith "" 0)
+
+"Test 4"
+(string-ith "" 2)
