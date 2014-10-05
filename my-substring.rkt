79bb73739d50a62acc6a83f4394db69425f06f93
Bradley M. Kuhn <bkuhn@ebb.org>
2014-10-05 16:53:38 -0400

    We decided to experiment with the order of recursion vs. string building in an attempt to fix the "order" of the string (our output looked backwards, string-wise) This seems to have fixed that particular problem (but the code still doesn't work).

--- my-substring.rkt
+++ my-substring.rkt
@@ -15,8 +15,8 @@
 
 (define my-substring-helper (lambda (str num1 num2)
    (cond [ (equal? num2 num1) (string-ith str num2)]   
-         [ else (string-append 
-                (my-substring-helper str (+ 1 num1) num2) (string-ith str (- num1 1)) ]
+         [ else (string-append (string-ith str (- num1 1))
+                (my-substring-helper str (+ 1 num1) num2))  ]
          )))
 
 (my-substring "hello world" 0 0)
