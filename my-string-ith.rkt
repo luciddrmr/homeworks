85a9975399978e0db41a9a9d4049f0005e044f10
Carol <carol@fossygirl.com>
2014-10-04 19:07:05 -0700

    Fixing errors

--- my-string-ith.rkt
+++ my-string-ith.rkt
@@ -7,11 +7,11 @@
 (define my-string-ith (lambda (str num)
     (cond  [ (equal? 0 (string-length str)) str ]
            [ (equal? 0 num) (substring str 0 1 ) ]
-           [ (equal? 1 (string-length str)) "error: my-string-ith: expected an exact integer in [0, 1) (i.e., less than the length of the given string) for the second argument, but received 1" ]
+           [ (and (equal? 1 num) (equal? 1 (string-length str))) "error: my-string-ith: expected an exact integer in [0, 1) (i.e., less than the length of the given string) for the second argument, but received 1" ]
            [ else (my-string-ith-helper str)]))
 
 (define my-string-ith-helper (lambda (str)
-    (cond  
+    (cond  [                         ]
            [ test2 another-base-case ]
            [ else (my-string-ith-helper str (substring str num (+1 num)) ]))))
 
